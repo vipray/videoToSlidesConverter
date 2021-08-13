b611f4a99f0d085320bab1be80228eb0c792b0e3
cidacoder <viprayjain18@gmail.com>
2021-08-14 00:25:12 +0530

    PDF code

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 ## Description
 - It takes video as input and extract different sildes(Images) from the video.
+- Then It convert images into PDF.
 - It Use structural_similarity to check similarity between two frames.
 - Two varaibles can be varied as per your specific case:
-    - var "threshold" can be 
+    - var "threshold" (depends on amount of change between two frames) can be 
         - decreased if getting False Positives(getting unnecessary images).
         - increased if missing True Positives(missing usefull images).
-    - var "skipBy" can be
+    - var "skipBy" (depends on speed of slides) can be
         - decreased if missing True Positives(missing usefull images).
         - increased if getting False Positives(getting unnecessary images).
+
+## Steps
+- Install required libraries using
+    - pip install -r requirements.txt
+    - If something else is missing, please download it as well using help of stackoverflow ;)
+- Script Run Command
+    - python script.py Sample.mp4
+    - replace "Sample.mp4" with <your file's name>
+    - it will create a PDF named <your file's name>.pdf
--- script.py
+++ script.py
@@ -3,12 +3,13 @@
 
 	Description:
 	- It takes video as input and extract different sildes(Images) from the video.
+	- Then It convert images into PDF.
 	- It Use structural_similarity to check similarity between two frames.
 	- Two varaibles can be varied as per your specific case:
-		- var "threshold" can be 
+		- var "threshold" (depends on amount of change between two frames) can be 
 			- decreased if getting False Positives.
 			- increased if missing True Positives.
-		- var "skipBy" can be
+		- var "skipBy" (depends on speed of slides) can be
 			- decreased if missing True Positives.
 			- increased if getting False Positives.
 
@@ -18,6 +19,10 @@
 # imports
 import cv2
 import sys
+import img2pdf
+import glob
+import os
+
 from skimage.metrics import structural_similarity as ssim
 
 #taking video name as a CLI argument.
@@ -43,7 +48,7 @@ oldGrayImage = None
 
 # "success" is true if vidcap.read() captured an Image
 if(success):
-	# Write Image into the folder
+	# Open below line to Write Image into the folder
 	cv2.imwrite("frame%d.jpg" % count, image)
 	
 	# Convert colored image into Gray image
@@ -69,6 +74,7 @@ while success:
 		
 		# Write Image into the folder
 	  	cv2.imwrite("frame%d.jpg" % count, image)
+	  	
 	  
 	  # Just To See 
 	  print('Read a new frame: ', success," ",simlarityIndex)
@@ -82,4 +88,23 @@ while success:
 	  # Skip skipBy frames
 	  # Checking all the frames doesn't help much, it just increase the Time Complexity.
 	  vidcap.set(cv2.CAP_PROP_POS_FRAMES, count)	  
-	  
+
+
+
+					
+''' Images to PDF conversion '''
+
+
+# List of all Images(name starts with 'frame')
+imageList = glob.glob("frame*.jpg")
+
+# Sort with respect to the Image number, so the PDF pages are in proper sequence
+sortedImageList = sorted(imageList, key = lambda x: (int(x.split("frame")[1].split(".jpg")[0]), x))
+
+# make a pdf from images using 'img2pdf' library
+with open(videoName.split(".")[0]+".pdf","wb") as f:
+	f.write(img2pdf.convert(sortedImageList))
+
+# delete all the images once the pdf is ready	
+for img in sortedImageList:
+	os.remove(img)
